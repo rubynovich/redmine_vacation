9ffe31ff23a07bb85a29b4035982619a7bad717b
rubynovich <roman@shipiev.me>
2013-03-20 17:42:22 +0400

    fix: vacation_mailer

--- app/models/vacation.rb
+++ app/models/vacation.rb
@@ -1,37 +1,37 @@
 class Vacation < ActiveRecord::Base
   unloadable
-  
+
   validates_presence_of :user_id
-  validates_uniqueness_of :user_id  
-  
+  validates_uniqueness_of :user_id
+
   belongs_to :user
-  belongs_to :last_planned_vacation, 
-    :class_name => 'VacationRange', 
+  belongs_to :last_planned_vacation,
+    :class_name => 'VacationRange',
     :foreign_key => 'last_planned_vacation_id'
-  belongs_to :active_planned_vacation, 
-    :class_name => 'VacationRange', 
+  belongs_to :active_planned_vacation,
+    :class_name => 'VacationRange',
     :foreign_key => 'active_planned_vacation_id'
-  belongs_to :not_planned_vacation, 
-    :class_name => 'VacationRange', 
+  belongs_to :not_planned_vacation,
+    :class_name => 'VacationRange',
     :foreign_key => 'not_planned_vacation_id'
 
   if Rails::VERSION::MAJOR >= 3
     scope :like_name, lambda {|q|
       if q.present?
-        {:conditions => 
-          ["LOWER(users.firstname) LIKE :p OR users.firstname LIKE :p OR LOWER(users.lastname) LIKE :p OR users.lastname LIKE :p", 
+        {:conditions =>
+          ["LOWER(users.firstname) LIKE :p OR users.firstname LIKE :p OR LOWER(users.lastname) LIKE :p OR users.lastname LIKE :p",
           {:p => "%#{q.to_s.downcase}%"}],
          :joins => :user}
       end
-    }  
+    }
   else
     named_scope :like_name, lambda {|q|
         if q.present?
-          {:conditions => 
-            ["LOWER(users.firstname) LIKE :p OR users.firstname LIKE :p OR LOWER(users.lastname) LIKE :p OR users.lastname LIKE :p", 
+          {:conditions =>
+            ["LOWER(users.firstname) LIKE :p OR users.firstname LIKE :p OR LOWER(users.lastname) LIKE :p OR users.lastname LIKE :p",
             {:p => "%#{q.to_s.downcase}%"}],
            :joins => :user}
         end
-      }    
+      }
   end
 end
--- app/models/vacation_mailer.rb
+++ app/models/vacation_mailer.rb
@@ -1,51 +1,93 @@
 class VacationMailer < Mailer
   unloadable
 
-  def from_author(user_id, issue_ids, vacation_range_id, author_id=nil)
-    user = User.find(user_id)
-    set_language_if_valid user.language
-    recipients user.mail
-    author = author_id && User.find(author_id) || vacation.user
-    vacation = VacationRange.find(vacation_range_id)    
-    subject l(:mail_subject_vacation_from_author, 
-      :user => author.name, 
-      :vacation_start => vacation.start_date.strftime("%d.%m.%Y"), 
-      :vacation_end => vacation.end_date.strftime("%d.%m.%Y"))
-    body :user => author, 
-         :issues => Issue.find(issue_ids),
-         :vacation => vacation
-    render_multipart('from_author', body)
-  end
+  if Rails::VERSION::MAJOR >= 3
+    def from_author(user_id, issue_ids, vacation_range_id, author_id=nil)
+      user = User.find(user_id)
+      set_language_if_valid user.language
+      recipients user.mail
+      author = author_id && User.find(author_id) || vacation.user
+      vacation = VacationRange.find(vacation_range_id)
+      subject l(:mail_subject_vacation_from_author,
+        :user => author.name,
+        :vacation_start => vacation.start_date.strftime("%d.%m.%Y"),
+        :vacation_end => vacation.end_date.strftime("%d.%m.%Y"))
+      body :user => author,
+           :issues => Issue.find(issue_ids),
+           :vacation => vacation
+      render_multipart('from_author', body)
+    end
 
-  def from_assigned_to(user_id, issue_ids, vacation_range_id, assigned_to_id=nil)
-    user = User.find(user_id) 
-    set_language_if_valid user.language
-    recipients user.mail
-    assigned_to = assigned_to_id && User.find(assigned_to_id) || vacation.user
-    vacation = VacationRange.find(vacation_range_id)
-    subject l(:mail_subject_vacation_from_assigned_to, 
-      :user => assigned_to.name, 
-      :vacation_start => vacation.start_date.strftime("%d.%m.%Y"), 
-      :vacation_end => vacation.end_date.strftime("%d.%m.%Y"))
-    body :user => assigned_to, 
-         :issues => Issue.find(issue_ids),
-         :vacation => vacation
-    render_multipart('from_assigned_to', body)
-  end
+    def from_assigned_to(user_id, issue_ids, vacation_range_id, assigned_to_id=nil)
+      user = User.find(user_id)
+      set_language_if_valid user.language
+      recipients user.mail
+      assigned_to = assigned_to_id && User.find(assigned_to_id) || vacation.user
+      vacation = VacationRange.find(vacation_range_id)
+      subject l(:mail_subject_vacation_from_assigned_to,
+        :user => assigned_to.name,
+        :vacation_start => vacation.start_date.strftime("%d.%m.%Y"),
+        :vacation_end => vacation.end_date.strftime("%d.%m.%Y"))
+      body :user => assigned_to,
+           :issues => Issue.find(issue_ids),
+           :vacation => vacation
+      render_multipart('from_assigned_to', body)
+    end
 
-  def render_multipart(method_name, body)
-    if Setting.plain_text_mail?
-      content_type "text/plain"
-      body render(:file => "#{method_name}.text.erb",
-                  :body => body,
-                  :layout => 'mailer.text.erb')
-    else
-      content_type "multipart/alternative"
-      part :content_type => "text/plain",
-           :body => render(:file => "#{method_name}.text.haml",
-                           :body => body, :layout => 'mailer.text.erb')
-      part :content_type => "text/html",
-           :body => render_message("#{method_name}.html.haml", body)
+    def render_multipart(method_name, body)
+      if Setting.plain_text_mail?
+        content_type "text/plain"
+        body render(:file => "#{method_name}.text.erb",
+                    :body => body,
+                    :layout => 'mailer.text.erb')
+      else
+        content_type "multipart/alternative"
+        part :content_type => "text/plain",
+             :body => render(:file => "#{method_name}.text.haml",
+                             :body => body, :layout => 'mailer.text.erb')
+        part :content_type => "text/html",
+             :body => render_message("#{method_name}.html.haml", body)
+      end
+    end
+  else
+    def from_author(user_id, issue_ids, vacation_range_id, author_id=nil)
+      user = User.find(user_id)
+      set_language_if_valid user.language
+      recipients = user.mail
+
+      @vacation = VacationRange.find(vacation_range_id)
+      @user = author_id && User.find(author_id) || @vacation.user
+      @issues = Issue.find(issue_ids)
+
+      subject = l(:mail_subject_vacation_from_author,
+        :user => @user.name,
+        :vacation_start => @vacation.start_date.strftime("%d.%m.%Y"),
+        :vacation_end => @vacation.end_date.strftime("%d.%m.%Y"))
+
+      mail(
+        :to => recipients,
+        :subject => subject
+      )
+    end
+
+    def from_assigned_to(user_id, issue_ids, vacation_range_id, assigned_to_id=nil)
+      user = User.find(user_id)
+      set_language_if_valid user.language
+      recipients = user.mail
+
+      @vacation = VacationRange.find(vacation_range_id)
+      @user = assigned_to_id && User.find(assigned_to_id) || @vacation.user
+      @issues = Issue.find(issue_ids)
+
+      subject = l(:mail_subject_vacation_from_assigned_to,
+        :user => @user.name,
+        :vacation_start => @vacation.start_date.strftime("%d.%m.%Y"),
+        :vacation_end => @vacation.end_date.strftime("%d.%m.%Y"))
+
+      mail(
+        :to => recipients,
+        :subject => subject
+      )
     end
   end
 end
