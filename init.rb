65a69a006121d170b64c0e9c87d87c38bec317fa
rubynovich <roman@shipiev.me>
2013-10-02 11:34:05 +0400

    del: menu

--- init.rb
+++ init.rb
@@ -8,20 +8,20 @@ Redmine::Plugin.register :redmine_vacation do
   url 'https://bitbucket.org/rubynovich/redmine_vacation'
   author_url 'http://roman.shipiev.me'
 
-  menu :application_menu, :vacations,
-    {:controller => :vacations, :action => :index},
-    :caption => :label_vacation_plural,
-    :if => Proc.new{ User.current.is_vacation_manager? }
+#  menu :application_menu, :vacations,
+#    {:controller => :vacations, :action => :index},
+#    :caption => :label_vacation_plural,
+#    :if => Proc.new{ User.current.is_vacation_manager? }
 
   menu :application_menu, :vacation_ranges,
     {:controller => :vacation_ranges, :action => :index},
     :caption => :label_vacation_range_plural,
     :if => Proc.new{ User.current.is_vacation_manager? }
 
-  menu :application_menu, :vacation_statuses,
-    {:controller => :vacation_statuses, :action => :index},
-    :caption => :label_vacation_status_plural,
-    :if => Proc.new{ User.current.is_vacation_manager? }
+#  menu :application_menu, :vacation_statuses,
+#    {:controller => :vacation_statuses, :action => :index},
+#    :caption => :label_vacation_status_plural,
+#    :if => Proc.new{ User.current.is_vacation_manager? }
 
   menu :admin_menu, :vacation_managers,
     {:controller => :vacation_managers, :action => :index}, :caption => :label_vacation_manager_plural, :html => {:class => :users}
