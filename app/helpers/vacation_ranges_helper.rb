08c6b9c3736ad576e27353cfdfc55803a096fc2a
rubynovich <roman@shipiev.me>
2013-10-10 10:29:45 +0400

    fix: nil.name

--- app/helpers/vacation_ranges_helper.rb
+++ app/helpers/vacation_ranges_helper.rb
@@ -3,25 +3,25 @@ module VacationRangesHelper
     decimal_separator = l(:general_csv_decimal_separator)
     export = FCSV.generate(:col_sep => l(:general_csv_separator)) do |csv|
       # csv header fields
-      csv << [ 
-        l(:field_user), 
+      csv << [
+        l(:field_user),
         l(:field_vacation_status),
         l(:field_description),
-        l(:field_vacation_start_date), 
+        l(:field_vacation_start_date),
         l(:field_vacation_end_date)
       ]
-      
+
       # csv lines
       @scope.all(:order => :start_date).each do |vacation_range|
         csv << [
-          vacation_range.user.name,
-          vacation_range.vacation_status.name,
+          vacation_range.user.to_s,
+          vacation_range.vacation_status.to_s,
           vacation_range.description,
           format_date(vacation_range.start_date),
           format_date(vacation_range.end_date)
         ]
       end
     end
-    export    
+    export
   end
 end
