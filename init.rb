8b7c266249f98627a50e5d3b2069439b50b41a02
Alexander Tkachev <teksisto@gmail.com>
2013-10-11 13:21:10 +0400

    add: top-menu reorganization. (see issue #58568)

--- init.rb
+++ init.rb
@@ -8,23 +8,42 @@ Redmine::Plugin.register :redmine_vacation do
   url 'https://bitbucket.org/rubynovich/redmine_vacation'
   author_url 'http://roman.shipiev.me'
 
-  menu :application_menu, :vacations,
-    {:controller => :vacations, :action => :index},
-    :caption => :label_vacation_plural,
-    :if => Proc.new{ User.current.is_vacation_manager? }
+  Redmine::MenuManager.map :top_menu do |menu| 
 
-  menu :application_menu, :vacation_ranges,
-    {:controller => :vacation_ranges, :action => :index},
-    :caption => :label_vacation_range_plural,
-    :if => Proc.new{ User.current.is_vacation_manager? }
+    unless menu.exists?(:hr)
+      menu.push(:hr, "#", 
+                { :after => :projects,
+                  :parent => :top_menu, 
+                  :caption => :label_hr_menu
+                })
+    end
 
-  menu :application_menu, :vacation_statuses,
-    {:controller => :vacation_statuses, :action => :index},
-    :caption => :label_vacation_status_plural,
-    :if => Proc.new{ User.current.is_vacation_manager? }
+    menu.push(:vacations, 
+              {:controller => :vacations, :action => :index},
+              { :parent => :hr,
+                :caption => :label_vacation_plural,
+                :if => Proc.new{ User.current.is_vacation_manager? }
+              })
+
+    menu.push(:vacation_ranges,
+              {:controller => :vacation_ranges, :action => :index},
+              { :parent => :hr,
+                :caption => :label_vacation_range_plural,
+                :if => Proc.new{ User.current.is_vacation_manager? }
+              })
+
+    menu.push(:vacation_statuses,
+              {:controller => :vacation_statuses, :action => :index},
+              { :parent => :hr,
+                :caption => :label_vacation_status_plural,
+                :if => Proc.new{ User.current.is_vacation_manager? }
+              })
+
+  end
 
   menu :admin_menu, :vacation_managers,
     {:controller => :vacation_managers, :action => :index}, :caption => :label_vacation_manager_plural, :html => {:class => :users}
+
 end
 
 Rails.configuration.to_prepare do
