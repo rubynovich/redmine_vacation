07b40c21ff60259a173c4598626102217491d6a8
rubynovich <roman@shipiev.me>
2013-10-30 11:35:20 +0400

    fix: issues#warning_flash

--- lib/vacation_issues_controller_patch.rb
+++ lib/vacation_issues_controller_patch.rb
@@ -4,33 +4,33 @@ module VacationPlugin
   module IssuesControllerPatch
     def self.included(base)
       base.extend(ClassMethods)
-      
+
       base.send(:include, InstanceMethods)
-      
+
       base.class_eval do
         before_filter :warning_flash, :only => [:create, :update]
         before_filter :set_attributes_before_change, :only => [:update]
       end
 
     end
-      
+
     module ClassMethods
     end
-    
+
     module InstanceMethods
       def set_attributes_before_change
         @issue.attributes_before_change = @attributes_before_change
       end
-      
+
       def warning_flash
         assigned_to_id = params[:issue][:assigned_to_id]
         if (vacation = Vacation.find_by_user_id(assigned_to_id))&&
           (vacation_range = vacation.active_planned_vacation)&&
-          (vacation_range.start_date > Date.today) #FIXME -> issue.start_date
-          
+          (vacation_range.start_date < issue.due_date + 1.month)
+
           flash[:warning] = t(:vacation_warning_flash,
             :name => User.find(assigned_to_id).name,
-            :from => vacation_range.start_date.strftime("%d.%m.%Y"), 
+            :from => vacation_range.start_date.strftime("%d.%m.%Y"),
             :to => vacation_range.end_date.strftime("%d.%m.%Y"))
         end
       end
