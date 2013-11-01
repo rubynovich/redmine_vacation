4988d84c2d454677e0cbce76d641770bb91558c0
rubynovich <roman@shipiev.me>
2013-11-01 09:13:46 +0400

    ops

--- lib/vacation_issues_controller_patch.rb
+++ lib/vacation_issues_controller_patch.rb
@@ -23,11 +23,10 @@ module VacationPlugin
         @issue.attributes_before_change = @attributes_before_change
       end
 
-      def warning_flash_for_create
-        assigned_to_id = params[:issue][:assigned_to_id]
-        due_date = Date.parse(params[:issue][:due_date])
+      def warning_flash(assigned_to_id, due_date)
         if (vacation = Vacation.find_by_user_id(assigned_to_id))&&
           (vacation_range = vacation.active_planned_vacation)&&
+          (vacation_range.start_date > Date.today)&&
           (vacation_range.start_date < due_date + 1.month)
 
           flash[:warning] = t(:vacation_warning_flash,
@@ -37,19 +36,17 @@ module VacationPlugin
         end
       end
 
+      def warning_flash_for_create
+        assigned_to_id = params[:issue][:assigned_to_id]
+        due_date = Date.parse(params[:issue][:due_date])
+        warning_flash(assigned_to_id, due_date)
+      end
+
       def warning_flash_for_update
         issue = Issue.find(params[:id])
-        assigned_to_id = issue.assigned_to_id
-        due_date = issue.due_date
-        if (vacation = Vacation.find_by_user_id(assigned_to_id))&&
-          (vacation_range = vacation.active_planned_vacation)&&
-          (vacation_range.start_date < due_date + 1.month)
-
-          flash[:warning] = t(:vacation_warning_flash,
-            :name => User.find(assigned_to_id).name,
-            :from => vacation_range.start_date.strftime("%d.%m.%Y"),
-            :to => vacation_range.end_date.strftime("%d.%m.%Y"))
-        end
+        assigned_to_id = params[:issue] && params[:issue][:assigned_to_id] || issue.assigned_to_id
+        due_date = params[:issue] && params[:issue][:due_date] && Date.parse(params[:issue][:due_date]) || issue.due_date
+        warning_flash(assigned_to_id, due_date)
       end
     end
   end
