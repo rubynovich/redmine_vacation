4f2c869df8b0c0f58f3aff26e7dfdda7b141cf79
Maksim <max@koritskiy.ru>
2014-06-10 15:35:16 +0400

    final check on_vacations

--- lib/vacation_meeting_agenda_patch.rb
+++ lib/vacation_meeting_agenda_patch.rb
@@ -36,16 +36,25 @@ module RedmineVacation
       end
 
 
+      def filter_users_on_vacation(users)
+        vacation_range_ids = VacationRange.where(["start_date <= ? and end_date >= ?",self.meet_on, self.meet_on]).map(&:id)
+        active_planned_vacation_user_ids = Vacation.where(["(active_planned_vacation_id is not null) and (active_planned_vacation_id in (?))", vacation_range_ids]).map(&:user_id)
+        last_planned_vacation_user_ids = Vacation.where(["(last_planned_vacation_id is not null) and (last_planned_vacation_id in (?))", vacation_range_ids]).map(&:user_id)
+        not_planned_vacation_user_ids = Vacation.where(["(not_planned_vacation_id is not null) and (not_planned_vacation_id in (?))", vacation_range_ids]).map(&:user_id)
+        User.where(:id => users.map(&:id)).where(["id not in (?)", [active_planned_vacation_user_ids, last_planned_vacation_user_ids, not_planned_vacation_user_ids].flatten.uniq.compact])
+      end
+
+
 
       def member_on_vacation?(user)
         if vacation = Vacation.find_by_user_id(user.id)
           if on_vacation?(vacation_range = vacation.active_planned_vacation) ||
               on_vacation?(vacation_range = vacation.last_planned_vacation) ||
               on_vacation?(vacation_range = vacation.not_planned_vacation)
-            return true
+            return vacation_range
           end
         end
-        return false
+        return nil
       end
 
       #def assigned_to_on_vacation_update
@@ -55,7 +64,8 @@ module RedmineVacation
       #  end
       #end
 
-      def on_vacation?(vacation_range)
+      def
+      on_vacation?(vacation_range)
         vacation_range.present? && self.meet_on.present?
           (vacation_range.nil? ? false : vacation_range.include?(self.meet_on))
       end
