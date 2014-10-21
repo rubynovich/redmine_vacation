3eb4f3d53fcbd6ca6fcffc29aacb378ebbf6b2d3
Alexander Tkachev <teksisto@gmail.com>
2014-10-21 13:21:15 +0400

    fix: показывать мертвых пользователей

--- app/models/vacation.rb
+++ app/models/vacation.rb
@@ -5,33 +5,17 @@ class Vacation < ActiveRecord::Base
   validates_uniqueness_of :user_id
 
   belongs_to :user
-  belongs_to :last_planned_vacation,
-    :class_name => 'VacationRange',
-    :foreign_key => 'last_planned_vacation_id'
-  belongs_to :active_planned_vacation,
-    :class_name => 'VacationRange',
-    :foreign_key => 'active_planned_vacation_id'
-  belongs_to :not_planned_vacation,
-    :class_name => 'VacationRange',
-    :foreign_key => 'not_planned_vacation_id'
+  belongs_to :last_planned_vacation, :class_name => 'VacationRange', :foreign_key => 'last_planned_vacation_id'
+  belongs_to :active_planned_vacation, :class_name => 'VacationRange', :foreign_key => 'active_planned_vacation_id'
+  belongs_to :not_planned_vacation, :class_name => 'VacationRange', :foreign_key => 'not_planned_vacation_id'
 
-  if Rails::VERSION::MAJOR >= 3
-    scope :like_name, lambda {|q|
-      if q.present?
-        {:conditions =>
-          ["LOWER(users.firstname) LIKE :p OR users.firstname LIKE :p OR LOWER(users.lastname) LIKE :p OR users.lastname LIKE :p",
-          {:p => "%#{q.to_s.downcase}%"}],
-         :joins => :user}
-      end
-    }
-  else
-    named_scope :like_name, lambda {|q|
-        if q.present?
-          {:conditions =>
-            ["LOWER(users.firstname) LIKE :p OR users.firstname LIKE :p OR LOWER(users.lastname) LIKE :p OR users.lastname LIKE :p",
-            {:p => "%#{q.to_s.downcase}%"}],
-           :joins => :user}
-        end
-      }
-  end
+  scope :like_name, lambda {|q|
+    if q.present?
+      {:conditions =>
+        ["LOWER(users.firstname) LIKE :p OR users.firstname LIKE :p OR LOWER(users.lastname) LIKE :p OR users.lastname LIKE :p",
+         {:p => "%#{q.to_s.downcase}%"}],
+        :joins => :user}
+    end
+  }
+
 end
